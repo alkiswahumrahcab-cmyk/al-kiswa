176a0b0b37589f1f6da03cc161f75f3c8038d95a
Al Kiswah Transport <alkiswahumrahcab@gmail.com>
2026-06-25 02:36:27 +0300

    fix: move TikTok Pixel to head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -167,15 +167,10 @@ export default async function RootLayout({
             })
           }}
         />
-      </head>
-      <body className={`${montserrat.variable} ${tajawal.variable}`}>
-        {settings.general.googleAnalyticsId && (
-          <GoogleAnalytics gaId={settings.general.googleAnalyticsId} />
-        )}
-
         {/* TikTok Pixel Code Start */}
-        <Script id="tiktok-pixel" strategy="afterInteractive">
-          {`
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
 !function (w, d, t) {
   w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
 var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
@@ -184,9 +179,15 @@ var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n
   ttq.load('D8U6E3JC77UER4V7P650');
   ttq.page();
 }(window, document, 'ttq');
-          `}
-        </Script>
+            `
+          }}
+        />
         {/* TikTok Pixel Code End */}
+      </head>
+      <body className={`${montserrat.variable} ${tajawal.variable}`}>
+        {settings.general.googleAnalyticsId && (
+          <GoogleAnalytics gaId={settings.general.googleAnalyticsId} />
+        )}
 
         <MenuProvider>
           <SettingsProvider>
